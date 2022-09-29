d8a4798df68eb3fc91b79bd41fe7eb2f0ea69752
Ian Anderson <ian033190@gmail.com>
2022-09-29 13:37:43 -0700

    R script
    this is the script to create the datasets

---
+++ Climatedata_pull.r
@@ -0,0 +1,32 @@
+
+
+arabidopsis_data <- read.csv("/Users/iananderson/Desktop/TerraClimate-1001Genome/availableplantsforclimate.csv",header = TRUE)
+
+# enter in variable you want to download see: http://thredds.northwestknowledge.net:8080/thredds/terraclimate_aggregated.html
+vars <- list("aet","def","pet","ppt","q","soil","srad","swe","tmax","tmin","vap","ws","vpd","PDSI")
+
+install.packages("ncdf4")
+library(ncdf4)
+
+for (i in vars) {
+  baseurlagg <- paste0(paste0("http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_terraclimate_",i),"_1958_CurrentYear_GLOBE.nc")
+  nc <- nc_open(baseurlagg)
+  # if you put opening the thing in the loop every time it takes forever forever. 
+  
+  
+  
+  for (j in 1:nrow(arabidopsis_data)) {
+    lon <- ncvar_get(nc, "lon")
+    lat <- ncvar_get(nc, "lat")
+    flat = match(abs(lat - j[2]) < 1/48, 1)
+    latindex = which(flat %in% 1)
+    flon = match(abs(lon - j[1]) < 1/48, 1)
+    lonindex = which(flon %in% 1)
+    start <- c(lonindex, latindex, 1)
+    count <- c(1, 1, -1)
+    
+    # read in the full period of record using aggregated files
+    
+    data <- as.numeric(ncvar_get(nc, varid = var,start = start, count))
+  }
+}
